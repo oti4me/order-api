69fa6af5d3a70d8e5aaf16e9cda7ac4f9b35b239
Henry Otighe <oti4me@Henry-Otighe-MacBook-Pro.local>
2020-12-24 13:54:31 +0100

    configure docker and testing

---
+++ test/app.spec.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import supertest from 'supertest'
+import app from '../src/server'
+
+const request = supertest(app)
+
+describe('GET: /api/v1', () => {
+  it('should return a 200 success status and a welcome message', (done) => {
+    request
+      .get('/api/v1')
+      .expect(200)
+      .end((err, { body }) => {
+        if (err) return done(err);
+        expect(body.message).to.equal('Welcome to the API for this great app.');
+        done();
+      });
+  });
+});
