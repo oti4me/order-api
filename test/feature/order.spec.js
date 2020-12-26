e8e8c6b21a10830de95579523cb695fb549db44a
Henry Otighe <oti4me@Henry-Otighe-MacBook-Pro.local>
2020-12-26 05:49:46 +0100

    add error logging to test

--- test/feature/order.spec.js
+++ test/feature/order.spec.js
@@ -7,11 +7,17 @@ const request = supertest(app);
 
 describe('GET: /api/v1/orders', () => {
   it('should return a 200 success status and a array of orders', async () => {
-    const { body } = await request
-      .get('/api/v1/orders')
-      .expect(200);
+    try {
+      const { body } = await request
+        .get('/api/v1/orders')
+        .expect(200);
 
-    expect(body.data[0]).to.haveOwnProperty('bookingDate');
-    expect(body.data[0]).to.haveOwnProperty('title');
+      expect(body.data[0]).to.haveOwnProperty('bookingDate');
+      expect(body.data[0]).to.haveOwnProperty('title');
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+
   });
 });
