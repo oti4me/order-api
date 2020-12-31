73cb00009c3d012e0a0b754ea9b03264910afd27
Henry Otighe <oti4me@Henry-Otighe-MacBook-Pro.local>
2020-12-31 03:58:50 +0100

    dig into the error object

--- test/mockData/order.js
+++ test/mockData/order.js
@@ -28,7 +28,7 @@ export const getToken = async () => {
       returnSecureToken: true
     });
     return data.idToken;
-  } catch ({ response }) {
-    return console.log(response.data);
+  } catch (error) {
+    return console.log(error);
   }
 };
